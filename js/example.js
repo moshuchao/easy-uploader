28d406ab954cd4dc68f9922a3b4af22c053eb014
moshuchao <chaoshumo@gmail.com>
2019-12-10 17:15:31 +0800

    💄 add css file

--- js/example.js
+++ js/example.js
@@ -1,20 +1,33 @@
 var $file = document.getElementById('file');
+var $imagesContainer = document.getElementById('images-container');
+var imageTpl = document.getElementById('image-tpl').innerHTML;
+var $uploader = document.getElementById('uploader');
+
+var $progTextNodelist, $progNodelist;
+
+var uploadFiles = [];
+
 var uploader = new Uploader('/upload', {
-    onSuccess(files) {
-        console.log(files)
+    onSuccess(res) {
+        console.log(res)
     },
     onError(ev) {
         console.log(ev)
     },
-    onProgress(p) {
-        console.log(p)
+    onProgress(allProgress) {
+        allProgress.forEach(function (p, i) {
+            p = +(p * 100).toFixed(2) + '%';
+            $progTextNodelist[i].textContent = p;
+            $progNodelist[i].style.width = p;
+        })
     },
     headers: {
         'token': 'anything',
     }
 });
-$file.onchange = function (ev) {
-    if (!this.files.item(0)) return;
+
+var onSelectImageChange = function (files) {
+    if (!files.item(0)) return;
 
     var createReader = function (file) {
         return new Promise(function (resolve, reject) {
@@ -32,8 +45,33 @@ $file.onchange = function (ev) {
         });
     }
 
-    var promises = Array.from(this.files).map(createReader);
-    Promise.all(promises).then(function (files) {
-        uploader.submit(files);
+    var promises = Array.from(files).map(createReader);
+
+    Promise.all(promises).then(function (md5Files) {
+        var uploadImage = md5Files.map(function (file, i) {
+            return imageTpl
+                .replace('{id}', file.uploadId)
+                .replace('{src}', window.URL.createObjectURL(file.input))
+                .replace('{alt}', file.input.name)
+                .replace('{size}', (file.input.size / 1024).toFixed(2) + 'kb')
+                .trim()
+        });
+
+        $uploader.insertAdjacentHTML('beforebegin', uploadImage.join(''))
+        $progTextNodelist = document.querySelectorAll('.uploader-file_progress_text');
+        $progNodelist = document.querySelectorAll('.uploader-file_progress_inner');
+
+        uploadFiles = uploadFiles.concat(md5Files);
     })
+}
+
+
+var revokeObjectURL = function (url) {
+    window.URL.revokeObjectURL(url);
+}
+
+var submitImages = function () {
+    if (uploadFiles.length > 0) {
+        uploader.submit(uploadFiles);
+    }
 }
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use('/dist', express.static('dist'));
 app.use('/js', express.static('js'));
+app.use('/css', express.static('css'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
