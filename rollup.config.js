33fd081f9d850e3d32b85768e320298c06c2e9b9
moshuchao <chaoshumo@gmail.com>
2019-12-12 14:25:54 +0800

    💚 fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-// import path from 'path';
+import { uglify } from "rollup-plugin-uglify";
+import { version } from './package.json';
+
+const banner = '/*! uploader.js version ' + version + ' */';
 
 export default {
     input: 'src/uploader.ts',
@@ -10,16 +11,21 @@ export default {
         file: 'dist/uploader.umd.js',
         format: 'umd',
         name: 'Uploader',
+        banner,
     }, {
         file: 'dist/uploader.cjs.js',
         format: 'cjs',
+        banner,
     }],
-    plugins: [resolve({
-        // 将自定义选项传递给解析插件
-        customResolveOptions: {
-            moduleDirectory: 'node_modules'
+    plugins: [typescript(), uglify({
+        output: {
+            comments: function (node, comment) {
+                if (comment.type === 'comment2') {
+                    // multiline comment
+                    return /@preserve|@license|@cc_on|^!/i.test(comment.value);
+                }
+                return false;
+            }
         }
-    }), commonjs({
-        include: /node_modules/,
-    }), typescript()],
+    })],
 };
