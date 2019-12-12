339b0372d632cfaf421096759f9e992d5a570a60
moshuchao <chaoshumo@gmail.com>
2019-12-12 14:33:32 +0800

    🏷️ update upload.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,34 @@
-declare module "uploader" {
-    interface IReq {
-        uploadId: string;
-        input: File;
-    }
-    interface IOption {
-        partSize?: number;
-        onError?: (ev: any) => void;
-        onProgress?: (p: number[]) => void;
-        onSuccess?: (res: any[]) => void;
-        headers: {
-            [name: string]: string;
-        };
-    }
-    export default class Uploader {
-        private url;
-        private partSize;
-        private onError;
-        private onProgress;
-        private onSuccess;
-        private progress;
-        private res;
-        constructor(url: string, opt?: IOption);
-        submit(files: IReq[]): void;
-    }
+interface IReq {
+    uploadId: string;
+    input: File;
+}
+interface IOption {
+    partSize?: number;
+    onError?: (ev: any) => void;
+    onProgress?: (p: {
+        [id: string]: number;
+    }) => void;
+    onSuccess?: (res: any[]) => void;
+    parallel?: number;
+    headers?: {
+        [name: string]: string;
+    };
+}
+export default class Uploader {
+    private url;
+    private progress;
+    private opt;
+    private res;
+    private _xhrs;
+    private _abortedFiles;
+    constructor(url: string, opt?: IOption);
+    getLoadedFile(): {
+        [id: string]: number;
+    };
+    isLoaded(id: string, index: number): boolean;
+    setLoadedFile(id: string, index: number): void;
+    removeLoadedFile(id: string): void;
+    abortAll(): void;
+    abort(id: string): void;
+    submit(files: IReq[]): void;
 }
