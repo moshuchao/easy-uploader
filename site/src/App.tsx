b80d59f1c96ff886301656a3e4f1fcf5586ff620
moshuchao <chaoshumo@gmail.com>
2020-05-29 15:43:34 +0800

    🎨 update

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState, useCallback, ChangeEvent, useRef, FormEvent } from 'react';
+import Uploader from '../../src/uploader';
+
+const uploader = new Uploader('http://localhost:3000/api/upload-files');
+
+uploader.on('success', (res) => {
+    console.log(res);
+});
+
+uploader.on('progress', (progress) => {
+
+    console.log(progress);
+});
+
+const FileItem = (props: { file: File }) => (
+    <li>
+        <div>
+            <span>{props.file.name}</span>
+            <button>cancel</button>
+        </div>
+        <div></div>
+    </li>
+);
+
 
 export default () => {
+    const [files, setFiles] = useState<File[]>([]);
+    const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target && e.target.files?.length) {
+            setFiles(Array.from(e.target.files));
+        }
+    }, []);
+
+    const onSubmit = useCallback((e: FormEvent) => {
+        e.preventDefault();
+        uploader.submit({ id: 'foo', file: files[0] });
+    }, [files]);
+
+
     return (
-        <div>1</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="file" multiple onChange={onChange} />
+                <ol>
+                    {files.map((file, i) => <FileItem file={file} key={i} />)}
+                </ol>
+                <button type="submit">submit</button>
+            </form>
+        </div>
     )
 }
