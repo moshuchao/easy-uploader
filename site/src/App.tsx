261645b05199d0c0012022b311e8f05aee204a66
moshuchao <chaoshumo@gmail.com>
2020-06-02 11:29:26 +0800

    ✨ add onCacel action

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -6,7 +6,7 @@ const uploader = new Uploader(process.env.REMOTE_HOST + '/api/upload-files', {
     partSize: +process.env.PARTSIZE
 });
 
-const FileItem = (props: { index: number, file: File, progress: number, url: string }) => (
+const FileItem = (props: { index: number, file: File, progress: number, url: string, onCancel: () => void }) => (
     <li className="file-item">
         <div className="file-item-body">
             <em>{props.index + 1}.</em>
@@ -14,7 +14,7 @@ const FileItem = (props: { index: number, file: File, progress: number, url: str
             {props.url
                 ? <a className="btn btn-link" href={process.env.REMOTE_HOST + props.url} target="_blank" rel="noopener noreferrer">download</a>
                 :
-                <button className="btn btn-warning">cancel</button>
+                <button type="button" className="btn btn-warning" onClick={props.onCancel}>cancel</button>
             }
         </div>
         <div className="progress" style={{ width: (props.progress * 100).toFixed(2) + '%' }}></div>
@@ -49,6 +49,14 @@ export default () => {
         uploader.onsuccess = res => setFileUrlMap({ ...res });
     }, []);
 
+    const onCancel = useCallback((index: number, id: string) => {
+        uploader.abort(id);
+        setUploadedData(prevState => {
+            const copy = [...prevState];
+            copy.splice(index, 1);
+            return copy
+        })
+    }, []);
 
     return (
         <div>
@@ -58,7 +66,15 @@ export default () => {
                     <span className="uploader-text">Chooses your files</span>
                 </label>
                 <ol className="uploader-list">
-                    {uploadedData.map((item, i) => <FileItem index={i} file={item[0]} key={item[1]} progress={progress[item[1]]} url={fileUrlMap[item[1]]} />)}
+                    {uploadedData.map((item, i) => (
+                        <FileItem
+                            index={i}
+                            file={item[0]}
+                            key={item[1]}
+                            progress={progress[item[1]]} url={fileUrlMap[item[1]]}
+                            onCancel={() => onCancel(i, item[1])}
+                        />
+                    ))}
                 </ol>
                 <button type="submit" className="btn btn-primary">submit</button>
             </form>
