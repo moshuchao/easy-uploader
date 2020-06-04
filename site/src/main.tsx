ead0801ba824fa62fe03474577e4c98ed294559e
moshuchao <chaoshumo@gmail.com>
2020-06-04 15:52:33 +0800

    :alien: remove hack function

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -2,20 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-function arrayBufferHack(this: Blob) {
-    return new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.onload = () => {
-            resolve(reader.result);
-        }
-
-        reader.readAsArrayBuffer(this)
-    })
-}
-
-File.prototype.arrayBuffer = File.prototype.arrayBuffer || arrayBufferHack;
-Blob.prototype.arrayBuffer = Blob.prototype.arrayBuffer || arrayBufferHack;
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if ((module as any).hot) {
